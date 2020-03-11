450ab71d01d5cf26cf286325ea12e1e95c656304
Felipe Marinho <felipmarinho@gmail.com>
2020-03-11 06:10:54 -0300

    implementing operators logic

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -13,7 +13,6 @@ const initialState = {
 }
 
 export default class Calculator extends Component {
-
 	state = { ...initialState }
 
 	constructor(props) {
@@ -28,7 +27,28 @@ export default class Calculator extends Component {
 	}
 
 	setOperation(operation) {
-		console.log(operation);
+		if (this.state.current === 0) {
+			this.setState({ operation, current: 1, clearDisplay: true })
+		} else {
+			const equals = operation === '='
+			const currentOperation = this.state.operation
+
+			const values = [...this.state.values]
+			try {
+				values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+			} catch (error) {
+				values[0] = this.state.values[0]
+			}
+			values[1] = 0
+
+			this.setState({ 
+				displayValue: values[0], 
+				operation: equals ? null : operation,
+				current: equals ? 0 : 1,
+				clearDisplay: !equals,
+				values
+			})
+		}
 	}
 
 	addDigit(n) {
@@ -37,7 +57,7 @@ export default class Calculator extends Component {
 		}
 
 		const clearDisplay = this.state.displayValue === '0'
-			|| this.clearDisplay
+			|| this.state.clearDisplay
 			const currentValue = clearDisplay ? '' : this.state.displayValue
 			const displayValue = currentValue + n
 			this.setState({ displayValue, clearDisplay: false })
@@ -48,11 +68,8 @@ export default class Calculator extends Component {
 				const values = [...this.state.values]
 				values[i] = newValue
 				this.setState({ values })
-				console.log(values);
-				
 			}
 	}
-
 
 	render() {
 		return (
